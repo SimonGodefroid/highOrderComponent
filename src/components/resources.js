e6b9bd6710b8f0780cc2351585e9131a48357f17
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-29 16:54:38 +0100

    created action creator, type and reducer

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<div>
+			Super Special Secret Recipe
+			<ul>
+				<li>1 Cup sugar</li>
+				<li>1 Cup pepper</li>
+				<li>1 Cup mint</li>
+			</ul>
+		</div>
+	);
+};
